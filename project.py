2c0ad76f5d984c036d1a0205fdb94507f0857a68
BasobPragyan <pragyan95prabodhbanerjee@gmail.com>
2024-10-31 21:24:51 +0530

    Due calculator

---
+++ project.py
@@ -0,0 +1,7 @@
+def due_amount(num1,num2):
+    print("Amount is ",num1)
+    print("Amount paid is ",num2)
+    print("Due amount is ",num1-num2)
+num1=int(input("Enter amount : "))
+num2=int(input("Enter paid amount : "))
+due_amount(num1,num2)
